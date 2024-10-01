4de74a9e854615562edaa99aac05b480e7d93711
Octávio <screminoctavio@gmail.com>
2024-09-30 22:53:52 -0300

    aprendendo comandos

--- projeto1/aula1.js
+++ projeto1/aula1.js
@@ -1,2 +1,9 @@
-var nome = "Octavio";
+"use strict";
+const nome = "Octavio";
 console.log("Bem-vindo " + nome);
+class Sistema {
+    constructor(ip) {
+        this.ip = ip;
+    }
+}
+const servidor1 = new Sistema("192.168.500.3");
--- projeto1/aula1.ts
+++ projeto1/aula1.ts
@@ -1,3 +1,13 @@
 const nome = "Octavio";
 
 console.log("Bem-vindo " + nome);
+
+class Sistema{
+    ip: string;
+
+    constructor(ip: string){
+        this.ip = ip;
+    }
+}
+
+const servidor1 = new Sistema("192.168.500.3")
